1f4efd7dc9e46f4b958af90139d15420216f617c
zhang-guodong <gd.zhang@live.com>
2020-09-20 21:00:08 +0800

    sync the file on ubuntu

---
+++ setubuntu.sh
@@ -0,0 +1,31 @@
+
+# set time 
+timedatectl set-local-rtc 1 --adjust-system-clock
+
+# install vscode
+sudo snap install --classic code
+
+# install qv2ray
+sudo snap install qv2ray
+
+# add ukylin source
+sudo add-apt-repository ppa:ubuntukylin-members/ukui3.0
+sudo apt upgrade
+
+# install sougoupinyin
+sudo apt install sogouimebs
+# sougoupinyin setup
+# sogouIme-configtool
+# TODO: need command to set up Fcitx
+
+# install git
+sudo apt install git
+# setup git
+git config --global user.name "Your Name"
+git config --global user.email "email@example.com"
+# TODO: need to setup user's name and email
+ssh-keygen -t rsa -C "youremail@example.com"
+# TODO: copy id_rsa.pub to github
+
+# download jetbrains toolbox
+# https://www.jetbrains.com/toolbox-app/download/download-thanks.html
